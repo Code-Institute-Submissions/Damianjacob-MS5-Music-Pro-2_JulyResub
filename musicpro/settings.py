6bb3d15d8be22f7e1695a2e6cf189f2103799308
Damian Jacob <damianm.jacob@yahoo.com>
2022-05-12 18:40:22 +0100

    Update settings to fix Server Error 500 with DEBUG=False

--- musicpro/settings.py
+++ musicpro/settings.py
@@ -28,6 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = False
 DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ms5-music-pro.herokuapp.com', 'localhost', '127.0.0.1']
@@ -139,18 +140,21 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_ACCESS_KEY_ID')
 S3_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME')
 AWS_S3_FILE_OVERWRITE = False  
 
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+
 AWS_DEFAULT_ACL = None  
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-AWS_S3_REGION_NAME = "eu-west-2"  
+AWS_S3_REGION_NAME = "eu-west-1"  
 
 # AWS_S3_ADDRESSING_STYLE = "virtual"
 
