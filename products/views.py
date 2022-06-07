15989dcd0a23650312177e888a9ee9d6b12cd6c8
Damian Jacob <damianm.jacob@yahoo.com>
2022-06-07 17:24:09 +0100

    Add sorting functionality to products page

--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from django.core.paginator import Paginator
 from django.contrib import messages
 from django.db.models import Q
 from .models import Product, Category
+from django.db.models.functions import Lower
 
 
 def products(request):
@@ -11,8 +12,25 @@ def products(request):
     products = Product.objects.all()
     query = None
     category = None
+    sort = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            print(f'sort: {sortkey}')
+            sort = sortkey
+            if sortkey == 'price_lth':
+                sortkey = 'price'
+                sort = 'Price - Low to High'
+            if sortkey == 'price_htl':
+                sortkey = '-price'
+                sort = 'Price - High to Low'
+            if sortkey == 'avg_customer_rating':
+                sortkey = '-rating'
+                sort = 'Average Customer Rating'
+            products = products.order_by(sortkey)
+
+
         if 'category' in request.GET:
             products = products.filter(category__name__iexact=request.GET['category'])
             category = get_object_or_404(Category, name=request.GET['category'])
@@ -38,6 +56,7 @@ def products(request):
         'page_obj': page_obj,
         'search_term': query,
         'current_category': category,
+        'current_sorting': sort,
     }
     return render(request, 'products/products.html', context)
 
