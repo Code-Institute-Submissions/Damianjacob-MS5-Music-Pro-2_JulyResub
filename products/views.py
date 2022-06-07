8914d218ad4dc9185ef0a9ef9519369aa66cf6a9
Damian Jacob <damianm.jacob@yahoo.com>
2022-06-07 11:19:48 +0100

    Add search functionality to products view

--- products/views.py
+++ products/views.py
@@ -1,16 +1,36 @@
-from django.shortcuts import get_object_or_404, render
-from .models import Product, Category
+from django.shortcuts import get_object_or_404, render, redirect, reverse
 from django.core.paginator import Paginator
+from django.contrib import messages
+from django.db.models import Q
+from .models import Product, Category
+
 
 def products(request):
     """Returns the products page"""
-    all_products = Product.objects.all()
-    paginator = Paginator(all_products, 36)
 
+    products = Product.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "Please enter a search keyword!")
+                return redirect(reverse('products'))
+            
+            queries = (
+                Q(name__icontains=query) |
+                Q(description__icontains=query) |
+                Q(category__name__icontains=query)
+            )
+            products = products.filter(queries)
+
+    paginator = Paginator(products, 36)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
     context = {
-        'page_obj': page_obj
+        'page_obj': page_obj,
+        'search_term': query,
     }
     return render(request, 'products/products.html', context)
 
