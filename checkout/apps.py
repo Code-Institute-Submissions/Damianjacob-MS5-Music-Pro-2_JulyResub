49a81382afeec0830dff8bd8abd7f912b76f0124
Damian Jacob <damianm.jacob@yahoo.com>
2022-06-22 11:20:55 +0100

    Add checkout signals and form

--- checkout/apps.py
+++ checkout/apps.py
@@ -1,6 +1,9 @@
 from django.apps import AppConfig
 
 
-class Test1Config(AppConfig):
+class CheckoutConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'test1'
+    name = 'checkout'
+
+    def ready(self):
+        import checkout.signals
