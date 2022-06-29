dad9a3c0000025f6929117cfe52e97f63219d0e1
Damian Jacob <damianm.jacob@yahoo.com>
2022-06-29 01:25:55 +0100

    Add newsletter-email model and update view logic

--- newsletter/models.py
+++ newsletter/models.py
@@ -1,3 +1,9 @@
+import email
 from django.db import models
 
 # Create your models here.
+class NewsletterEmail(models.Model):
+    email = email = models.EmailField(max_length=254, null=False, blank=False)
+
+    def __str__(self):
+        return email
--- newsletter/views.py
+++ newsletter/views.py
@@ -2,6 +2,8 @@ from django.contrib import messages
 from django.shortcuts import render
 from django.core.mail import send_mail
 
+from .models import NewsletterEmail
+
 
 def newsletter(request):
     template = 'newsletter.html'
@@ -9,18 +11,25 @@ def newsletter(request):
 
     if request.method == 'POST':
         email = request.POST.get('newsletter-email')
+
         try:
-            send_mail(
-                'Musicpro Newsletter Confirmation',
-                'Thank you for subscribing to our newsletter!',
-                'newsletter@musicpro.com',
-                [email],
-                fail_silently=False,
-            )
-            messages.info(request, f'You have successfully subscribed to our newsletter!\
-                    A confirmation email has been sent to {email}')
-        except Exception as e:
-            messages.error(request, f'Sorry, there has been an error: {e}')
+            existing_email = NewsletterEmail.objects.get(email=email)
+            if existing_email:
+                messages.info(request, 'You are already subscribed to our newsletter!')
+        except NewsletterEmail.DoesNotExist:
+            try:
+                send_mail(
+                    'Musicpro Newsletter Confirmation',
+                    'Thank you for subscribing to our newsletter!',
+                    'newsletter@musicpro.com',
+                    [email],
+                    fail_silently=False,
+                )
+                messages.info(request, f'You have successfully subscribed to our newsletter!\
+                        A confirmation email has been sent to {email}')
+                NewsletterEmail.objects.create(email=email)
+            except Exception as e:
+                messages.error(request, f'Sorry, there has been an error: {e}')
 
 
     return render(request, template_name=template, context=context)
